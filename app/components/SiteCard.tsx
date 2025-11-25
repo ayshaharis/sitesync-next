232cde5c0cbd62e01abdac90b423a5024d031009
aysha <ayshaharis029@gmail.com>
2025-11-25 22:32:05 +0530

    site grid and site card component added

---
+++ app/components/SiteCard.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+interface SiteCardProps {
+    id:string|number;
+    name?:string;
+    location?:string;
+    status?:string;
+    onClick?: ()=>void;
+    isAddCard?:boolean;
+
+}
+
+const SiteCard:React.FC<SiteCardProps>=({
+    id,
+    name,
+    location,
+    status,
+    onClick,
+    isAddCard=false
+})=>{
+    if(isAddCard){
+        return(
+             <div
+        className="p-6 bg-green-100 border border-green-600 rounded-xl shadow cursor-pointer flex items-center justify-center text-green-700 font-semibold text-xl"
+        onClick={onClick}
+      >
+        + Add New Site
+      </div>
+        )
+}
+
+return(
+    <div className="p-6 bg-white border border-gray-300 rounded-xl shadow hover:shadow-lg transition-shadow cursor-pointer">
+      <h3 className="text-xl font-bold mb-2">Name:{name}</h3>
+      <p className="text-gray-600 mb-1">Location: {location}</p>
+      <p className="text-gray-600">Status: {status}</p>
+    </div>
+)
+}
+
+
+export default SiteCard;
--- app/components/SiteGrid.tsx
+++ app/components/SiteGrid.tsx
@@ -1,11 +1,35 @@
-import SiteCard from "./SiteCards";
+import SiteCard from "./SiteCard";
+import React from 'react'
+interface sites{
+    id:string|number;
+    name?:string;
+    location?:string;
+    status?:string;
+}
+interface SiteGridProps{
+    sites:sites[];
+    onAddNewSite:()=>void;
 
-export default function SiteGrid() {
+}
+const SiteGrid:React.FC<SiteGridProps>=({sites,onAddNewSite})=>{
+
+
     return(
-        <div>
-            <h2>Site Grid Component</h2>
-            <p>SiteCards.</p>
-            <SiteCard/>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-6">
+
+           <SiteCard id="add" name="Add New Site" onClick={onAddNewSite} isAddCard={true}/>
+        {(sites||[]).map((site,key)=>(
+            <SiteCard
+            key={site.id}
+            id={site.id}
+            name={site.name}
+            location={site.location}
+            status={site.status}
+            isAddCard={false}/>
+        ))}
+
         </div>
+     
     )
 }
+    export default SiteGrid
